fc1e958cd1633f0219cfbafc6ef21aba9bc74116
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-03-07 13:06:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 canvas = document.getElementById("Canvas");
 ctx = canvas.getContext("2d");
-background_img = "mars.jpg";
+var NASA=["https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG","https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RLB_486265291EDR_F0481570RHAZ00323M_.JPG","https://mars.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631300305227E03_DXXX.jpg","https://previews.123rf.com/images/cobalt/cobalt1907/cobalt190700030/127574708-mars-landscape-3d-render-of-imaginary-mars-planet-terrain-science-fiction-illustration-.jpg"];
+var RN = Math.floor(Math.random()*4);
+console.log (RN);
+background_img = NASA[RN];
+console.log(background_img);
 RoverImage = "rover.png";
 RovW=100;
 RovH=90;
@@ -25,26 +29,61 @@ function uploadRover() {
     ctx.drawImage(Ro1, RovX, RovY, RovW, RovH);
 }
 
-window.addEventListener("keydown", ASCII);
-function ASCII(e){
-    keyp=e.keycode;
-    if(keyp == "38") {
+window.addEventListener("keydown", I);
+function I(e){
+    keyp = e.keyCode;
+    if(keyp == '38') {
         up();
         console.log("up");
     }
 
-    if(keyp == "40") {
+    if(keyp == '40') {
         down();
         console.log("down");
     }
 
-    if(keyp == "37") {
+    if(keyp == '37') {
         left();
         console.log("left");
     }
 
-    if(keyp == "39") {
+    if(keyp == '39') {
         right();
         console.log("right");
+    }
+}
+function up(){
+    if (RovY >=0){
+        RovY = RovY-10;
+        uploadBackground();
+            uploadRover();
+            console.log("when up is pressed X="+RovX+" Y ="+RovY);
+    }
+}
+
+function down(){
+    if (RovY <=400){
+        RovY = RovY+10;
+        uploadBackground();
+            uploadRover();
+            console.log("when down is pressed X="+RovX+" Y ="+RovY);
+    }
+}
+
+function left(){
+    if (RovX >=0){
+        RovX = RovX-10;
+        uploadBackground();
+            uploadRover();
+            console.log("when left is pressed X="+RovX+" Y ="+RovY);
+    }
+}
+
+function right(){
+    if (RovX <=400){
+        RovX = RovX+10;
+        uploadBackground();
+            uploadRover();
+            console.log("when right is pressed X="+RovX+" Y ="+RovY);
     }
 }
